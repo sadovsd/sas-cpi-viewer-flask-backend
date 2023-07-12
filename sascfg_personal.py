fe6b86dcb443972e643e6571ffc325056098ec60
Davyd Sadovskyy <sadovsd@miamioh.edu>
2023-07-12 19:31:49 -0400

    add saspy connection files

---
+++ sascfg_personal.py
@@ -0,0 +1,9 @@
+SAS_config_names=['oda']
+
+oda = {'java' : '/usr/bin/java',
+#My SAS onDemand 'US Home Region 2'
+'iomhost' : ['odaws01-usw2-2.oda.sas.com','odaws02-usw2-2.oda.sas.com'],
+'iomport' : 8591,
+'authkey' : 'oda',
+'encoding' : 'utf-8'
+}
